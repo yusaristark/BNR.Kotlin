ff4e3ea0618b30b9ff21b5f9edb8b695902af180
yusaristark <yusaristark@gmail.com>
2021-10-02 19:22:08 +0300

    wtf

--- NeytHack/src/main/kotlin/Game.kt
+++ NeytHack/src/main/kotlin/Game.kt
@@ -1,10 +1,23 @@
+import kotlin.math.pow
+
 fun main(args: Array<String>) {
     val name = "Madrigal"
-    var healthPoints = 89
+    val healthPoints = 89
     val isBlessed = true
     val isImmortal = false
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    val karma = (Math.random().pow((110 - healthPoints) / 100.0) * 20).toInt()
+    val auraColor = if (auraVisible) {
+        when (karma) {
+            in 0..5 -> "RED"
+            in 6..10 -> "ORANGE"
+            in 11..15 -> "PURPLE"
+            in 16..20 -> "GREEN"
+            else -> "NONE"
+        }
+    } else {
+        "NONE"
+    }
 
     val healthStatus = when (healthPoints) {
         100 -> "is in excellent condition!"
@@ -12,7 +25,7 @@ fun main(args: Array<String>) {
         in 75..89 -> if (isBlessed) {
             "has some minor wounds but is healing quite quickly!"
         } else {
-            "has some mino wounds."
+            "has some minor wounds."
         }
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
