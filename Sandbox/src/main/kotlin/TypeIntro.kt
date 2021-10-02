c7ce124e070e63d0b9f575422baa13a54645b28b
yusaristark <yusaristark@gmail.com>
2021-10-02 18:46:05 +0300

    first commit

---
+++ Sandbox/src/main/kotlin/TypeIntro.kt
@@ -0,0 +1,11 @@
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main() {
+    val playerName = "Estragon"
+    var experiencePoints: Int = 5
+    var hasSteed = false
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+    println(playerName.reversed())
+}
